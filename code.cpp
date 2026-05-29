0e34f5113be622a9fc6f23438297d67e9deda243
Vinayak S <vinayaksanthosh2005@gmail.com>
2026-05-29 16:38:55 +0530

    Updated code.cpp

--- code.cpp
+++ code.cpp
@@ -9,35 +9,34 @@ vector<vector<string>> minimizeCashFlow(vector<vector<string>>& transactions) {
    balance[t[1]] += amount;
     }
 
-  vector<pair<string, long long>> debtors;
- vector<pair<string, long long>> creditors;
+vector<pair<string, long long>> debtors;
+vector<pair<string, long long>> creditors;
 
-  for (auto& p : balance) {
+for (auto& p : balance) {
         if (p.second < 0)
             debtors.push_back({p.first, -p.second});
         else if (p.second > 0)
             creditors.push_back({p.first, p.second});
     }
 
-   vector<vector<string>> result;
+vector<vector<string>> result;
 
-   int i = 0, j = 0;
+int i = 0, j = 0;
 
-   while (i < debtors.size() && j < creditors.size()) {
-        long long amount = min(debtors[i].second, creditors[j].second);
-
-  result.push_back({
+while (i < debtors.size() && j < creditors.size()) {
+long long amount = min(debtors[i].second, creditors[j].second);
+result.push_back({
             debtors[i].first,
             creditors[j].first,
             to_string(amount)
         });
 
-   debtors[i].second -= amount;
-   creditors[j].second -= amount;
+debtors[i].second -= amount;
+creditors[j].second -= amount;
 
-  if (debtors[i].second == 0) i++;
-   if (creditors[j].second == 0) j++;
+if (debtors[i].second == 0) i++;
+if (creditors[j].second == 0) j++;
     }
 
-  return result;
+return result;
 }
